9775419dcc518b1df7bd2e660c8b16e3a417a1a2
hojunking96 <hojunking96@gmail.com>
2023-03-14 12:57:49 +0900

    plus

--- src/main/java/com/ll/basic1/HomeController.java
+++ src/main/java/com/ll/basic1/HomeController.java
@@ -1,7 +1,10 @@
 package com.ll.basic1;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+
 // @Controller 의 의미
 // 개발자가 스프링부트에게 말한다.
 // 아래 있는 HomeController 는 컨트롤러이다.
@@ -23,11 +26,13 @@ public class HomeController {
     public String showMain() {
         return "안녕하세요.";
     }
+
     @GetMapping("/home/main2")
     @ResponseBody
     public String showMain2() {
         return "반갑습니다.";
     }
+
     @GetMapping("/home/main3")
     @ResponseBody
     public String showMain3() {
@@ -39,5 +44,15 @@ public class HomeController {
     public int showIncrease() { // 리턴되는 int 값은 String 화 되어서 고객(브라우저)에게 전달된다.
         count++;
         return count;
+    }
+
+    @GetMapping("/home/plus")
+    @ResponseBody
+    // @RequestParam 의 의미
+    // 개발자가 스프링부트에게 말한다.
+    // int a 는 쿼리스트링에서 a 파라미터의 값을 얻은 후 정수화 한 값이어야 한다.
+    // @RequestParam 는 생략가능
+    public int showPlus(@RequestParam(defaultValue = "0") int a, @RequestParam int b) {
+        return a + b;
     }
 }
