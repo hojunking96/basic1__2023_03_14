938a9cc0a8b167187539bdc7d15a529ff1c4cf6e
hojunking96 <hojunking96@gmail.com>
2023-03-15 14:37:08 +0900

    로그인

--- src/main/java/com/ll/basic1/boundedContext/member/service/MemberService.java
+++ src/main/java/com/ll/basic1/boundedContext/member/service/MemberService.java
@@ -11,10 +11,11 @@ import org.springframework.stereotype.Service;
 @Service
 @AllArgsConstructor
 public class MemberService {
-    private MemberRepository memberRepository;
+    private final MemberRepository memberRepository;
 
     public RsData tryLogin(String username, String password) {
         Member member = memberRepository.findByUsername(username);
+
         if (member == null) {
             return RsData.of("F-2", "%s(은)는 존재하지 않는 회원입니다.".formatted(username));
         }
@@ -23,10 +24,14 @@ public class MemberService {
             return RsData.of("F-1", "비밀번호가 일치하지 않습니다.");
         }
 
-        return RsData.of("S-1", "%s 님 환영합니다.".formatted(username));
+        return RsData.of("S-1", "%s 님 환영합니다.".formatted(username), member.getId());
     }
 
     public Member findByUsername(String username) {
         return memberRepository.findByUsername(username);
+    }
+
+    public Member findById(long id) {
+        return memberRepository.findById(id);
     }
 }
